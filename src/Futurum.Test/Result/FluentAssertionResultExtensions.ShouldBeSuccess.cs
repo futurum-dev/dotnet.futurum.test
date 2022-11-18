72db6625adfd2d94924925e8bbfc4ea7cbbac823
futurum-dev <me@futurum.dev>
2022-11-18 17:56:09 +0000

    Adding 'safe' versions of FluentAssertionResultExtensions ShouldBeFailure extension methods

--- src/Futurum.Test/Result/FluentAssertionResultExtensions.ShouldBeSuccess.cs
+++ src/Futurum.Test/Result/FluentAssertionResultExtensions.ShouldBeSuccess.cs
@@ -8,7 +8,7 @@ namespace Futurum.Test.Result;
 /// <summary>
 /// Extension methods for <see cref="FluentAssertions"/>, so that it supports <see cref="Futurum.Core.Result.Result"/> and <see cref="Futurum.Core.Result.Result{T}"/>
 /// </summary>
-public static class FluentAssertionResultExtensions
+public static partial class FluentAssertionResultExtensions
 {
     /// <summary>
     /// Specifies that the <see cref="Futurum.Core.Result.Result"/> should be <see cref="Futurum.Core.Result.Result.IsSuccess"/> true.
@@ -160,71 +160,5 @@ public static class FluentAssertionResultExtensions
         result.IsSuccess.Should().BeTrue(errorMessage);
 
         valueAssertion(result.Value.Value);
-    }
-
-    /// <summary>
-    /// Specifies that the <see cref="Futurum.Core.Result.Result"/> should be <see cref="Futurum.Core.Result.Result.IsFailure"/> true.
-    /// </summary>
-    public static void ShouldBeFailure(this Core.Result.Result result)
-    {
-        result.IsFailure.Should().BeTrue();
-    }
-
-    /// <summary>
-    /// Specifies that the <see cref="Futurum.Core.Result.Result"/> should be <see cref="Futurum.Core.Result.Result.IsFailure"/> true with <paramref name="errorMessages"/>.
-    /// </summary>
-    public static void ShouldBeFailureWithError(this Core.Result.Result result, params string[] errorMessages)
-    {
-        result.ShouldBeFailure();
-
-        result.Error.Map(ResultErrorStringExtensions.ToErrorString).Should().Be(string.Join(";", errorMessages));
-    }
-
-    /// <summary>
-    /// Specifies that the <see cref="Futurum.Core.Result.Result{T}"/> should be <see cref="Futurum.Core.Result.Result{T}.IsFailure"/> true.
-    /// </summary>
-    public static void ShouldBeFailure<T>(this Result<T> result)
-    {
-        result.IsFailure.Should().BeTrue();
-    }
-
-    /// <summary>
-    /// Specifies that the <see cref="Futurum.Core.Result.Result{T}"/> should be <see cref="Futurum.Core.Result.Result{T}.IsFailure"/> true with <paramref name="errorMessages"/>.
-    /// </summary>
-    public static void ShouldBeFailureWithError<T>(this Result<T> result, params string[] errorMessages)
-    {
-        result.ShouldBeFailure();
-
-        result.Error.Map(ResultErrorStringExtensions.ToErrorString).Should().Be(string.Join(";", errorMessages));
-    }
-
-    /// <summary>
-    /// Specifies that the <see cref="Futurum.Core.Result.Result{T}"/> should be <see cref="Futurum.Core.Result.Result{T}.IsFailure"/> true containing <paramref name="errorMessage"/>.
-    /// </summary>
-    public static void ShouldBeFailureWithErrorContaining(this Core.Result.Result result, string errorMessage)
-    {
-        result.ShouldBeFailure();
-
-        result.Error.Value.Flatten().Any(e => e.ToErrorString(",").Contains(errorMessage))
-              .Should().BeTrue($"Error does not contain error message : '{errorMessage}' , '{result.Error.Value.ToErrorString(",")}'");
-    }
-
-    /// <summary>
-    /// Specifies that the <see cref="Futurum.Core.Result.Result{T}"/> should be <see cref="Futurum.Core.Result.Result{T}.IsFailure"/> true containing <paramref name="errorMessage"/>.
-    /// </summary>
-    public static void ShouldBeFailureWithErrorContaining<T>(this Result<T> result, string errorMessage)
-    {
-        result.ShouldBeFailure();
-
-        result.Error.Value.Flatten().Any(e => e.ToErrorString(",").Contains(errorMessage))
-              .Should().BeTrue($"Error does not contain error message : '{errorMessage}' , '{result.Error.Value.ToErrorString(",")}'");
-    }
-
-    /// <summary>
-    /// Specifies that the <see cref="Futurum.Core.Result.IResultError"/> should be <paramref name="errorMessages"/>.
-    /// </summary>
-    public static void ShouldBeError(this IResultError error, params string[] errorMessages)
-    {
-        error.ToErrorString().Should().Be(string.Join(";", errorMessages));
     }
 }
---
+++ test/Futurum.Test.Tests/Result/FluentAssertionResultExtensionsTests.ShouldBeFailure.cs
@@ -0,0 +1,271 @@
+using FluentAssertions;
+
+using Futurum.Core.Result;
+using Futurum.Test.Result;
+
+using Xunit;
+
+namespace Futurum.Test.Tests.Result;
+
+public class FluentAssertionResultExtensionsShouldBeFailureTests
+{
+    private const string ERROR_MESSAGE = "Error-Message";
+
+    public class ShouldBeFailure
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok();
+
+            var action = () => option.ShouldBeFailure();
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail(ERROR_MESSAGE);
+
+            option.ShouldBeFailure();
+        }
+    }
+
+    public class ShouldBeFailureWithErrorSafe
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok();
+
+            var action = () => option.ShouldBeFailureWithErrorSafe(ERROR_MESSAGE);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail(ERROR_MESSAGE);
+
+            option.ShouldBeFailureWithErrorSafe(ERROR_MESSAGE);
+        }
+    }
+
+    public class ShouldBeFailureWithError
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok();
+
+            var action = () => option.ShouldBeFailureWithError(ERROR_MESSAGE);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail(ERROR_MESSAGE);
+
+            option.ShouldBeFailureWithError(ERROR_MESSAGE);
+        }
+    }
+
+    public class ShouldBeFailure_Generic
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok(10);
+
+            var action = () => option.ShouldBeFailure();
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail<int>(ERROR_MESSAGE);
+
+            option.ShouldBeFailure();
+        }
+    }
+
+    public class ShouldBeFailureWithErrorSafe_Generic
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok(10);
+
+            var action = () => option.ShouldBeFailureWithErrorSafe(ERROR_MESSAGE);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail<int>(ERROR_MESSAGE);
+
+            option.ShouldBeFailureWithErrorSafe(ERROR_MESSAGE);
+        }
+    }
+
+    public class ShouldBeFailureWithError_Generic
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok(10);
+
+            var action = () => option.ShouldBeFailureWithError(ERROR_MESSAGE);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail<int>(ERROR_MESSAGE);
+
+            option.ShouldBeFailureWithError(ERROR_MESSAGE);
+        }
+    }
+
+    public class ShouldBeFailureWithErrorSafeContaining
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok();
+
+            var action = () => option.ShouldBeFailureWithErrorSafeContaining(ERROR_MESSAGE);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail(ResultErrorCompositeExtensions.ToResultError(ERROR_MESSAGE.ToResultError(), ERROR_MESSAGE.ToResultError()));
+
+            option.ShouldBeFailureWithErrorSafeContaining(ERROR_MESSAGE);
+        }
+    }
+
+    public class ShouldBeFailureWithErrorContaining
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok();
+
+            var action = () => option.ShouldBeFailureWithErrorContaining(ERROR_MESSAGE);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail(ResultErrorCompositeExtensions.ToResultError(ERROR_MESSAGE.ToResultError(), ERROR_MESSAGE.ToResultError()));
+
+            option.ShouldBeFailureWithErrorContaining(ERROR_MESSAGE);
+        }
+    }
+
+    public class ShouldBeFailureWithErrorSafeContaining_Generic
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok(10);
+
+            var action = () => option.ShouldBeFailureWithErrorSafeContaining(ERROR_MESSAGE);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail<int>(ResultErrorCompositeExtensions.ToResultError(ERROR_MESSAGE.ToResultError(), ERROR_MESSAGE.ToResultError()));
+
+            option.ShouldBeFailureWithErrorSafeContaining(ERROR_MESSAGE);
+        }
+    }
+
+    public class ShouldBeFailureWithErrorContaining_Generic
+    {
+        [Fact]
+        public void Success()
+        {
+            var option = Core.Result.Result.Ok(10);
+
+            var action = () => option.ShouldBeFailureWithErrorContaining(ERROR_MESSAGE);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should().Contain("Expected result.IsFailure to be true, but found False.");
+        }
+
+        [Fact]
+        public void Failure()
+        {
+            var option = Core.Result.Result.Fail<int>(ResultErrorCompositeExtensions.ToResultError(ERROR_MESSAGE.ToResultError(), ERROR_MESSAGE.ToResultError()));
+
+            option.ShouldBeFailureWithErrorContaining(ERROR_MESSAGE);
+        }
+    }
+
+    public class ShouldBeErrorSafe
+    {
+        [Fact]
+        public void success()
+        {
+            var resultError = ERROR_MESSAGE.ToResultError();
+
+            resultError.ShouldBeErrorSafe(ERROR_MESSAGE);
+        }
+
+        [Fact]
+        public void failure()
+        {
+            var incorrectErrorMessage = "test";
+
+            var resultError = ERROR_MESSAGE.ToResultError();
+
+            var action = () => resultError.ShouldBeErrorSafe(incorrectErrorMessage);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should()
+                  .Contain("Expected error.ToErrorStringSafe() to be \"test\" with a length of 4, but \"Error-Message\" has a length of 13, differs near \"Err\" (index 0).");
+        }
+    }
+
+    public class ShouldBeError
+    {
+        [Fact]
+        public void success()
+        {
+            var resultError = ERROR_MESSAGE.ToResultError();
+
+            resultError.ShouldBeError(ERROR_MESSAGE);
+        }
+
+        [Fact]
+        public void failure()
+        {
+            var incorrectErrorMessage = "test";
+
+            var resultError = ERROR_MESSAGE.ToResultError();
+
+            var action = () => resultError.ShouldBeError(incorrectErrorMessage);
+
+            action.Should().Throw<Xunit.Sdk.XunitException>().Which.Message.Should()
+                  .Contain("Expected error.ToErrorString() to be \"test\" with a length of 4, but \"Error-Message\" has a length of 13, differs near \"Err\" (index 0).");
+        }
+    }
+}
